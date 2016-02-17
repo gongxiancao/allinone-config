0433ccb1a6adc56e9eb1d89f0bd184d8743ca6f4
GongXian Cao <gongxiancao@hotmail.com>
2016-02-17 15:24:09 +0800

    use framework instance as this arg

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var pathUtil = require('path'),
   fs = require('fs');
 
-module.exports = function (app, done) {
-  var filePath, extname, stat,
+module.exports = function (done) {
+  var filePath, extname, stat, self = this;
     fileNames = fs.readdirSync(app.config.paths.config);
 
   fileNames.forEach(function (fileName) {
-    filePath = pathUtil.join(app.config.paths.config, fileName);
+    filePath = pathUtil.join(self.config.paths.config, fileName);
     stat = fs.statSync(filePath);
     extname = pathUtil.extname(filePath);
     if(stat && stat.isFile && extname === '.js') {
-      _.merge(app.config, require(filePath));
+      _.merge(self.config, require(filePath));
     }
   });
 
-  var filePath = pathUtil.join(app.config.paths.envConfig, app.environment);
-  _.merge(app.config, require(filePath));
+  var filePath = pathUtil.join(self.config.paths.envConfig, self.environment);
+  _.merge(self.config, require(filePath));
 
   process.nextTick(done);
 };
